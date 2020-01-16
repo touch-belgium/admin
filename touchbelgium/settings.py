970e5ac517800d45f83767e1ff4bcb60fc9891df
Carlos Requena López <crequena@ulb.ac.be>
2020-01-16 19:07:11 +0100

    Add Nuxt dev server address to CORS ORIGIN WHITELIST

--- touchbelgium/settings.py
+++ touchbelgium/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
+    'http://localhost:3000',
     'https://beta.touch-belgium.be',
     'https://touch-belgium.be'
 )
